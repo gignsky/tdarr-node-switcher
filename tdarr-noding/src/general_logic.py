7c8dbb03542e973ac60d87877ccb7ed7d49ed432
gignsky <gignsky@gmail.com>
2023-02-19 23:38:38 -0500

    updated missing constants per last commit

--- tdarr-noding/src/general_logic.py
+++ tdarr-noding/src/general_logic.py
@@ -3,6 +3,6 @@ from . import tdarr
 
 class Logic:
     @staticmethod
-    def refresh_all(Constants):
+    def refresh_all(constants):
         # tdarr.Tdarr_Orders.refresh_health_checks(constants)
         tdarr.Tdarr_Orders.update_failed_transcodes(constants)
