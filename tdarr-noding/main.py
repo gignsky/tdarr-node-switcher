d3eb8a2370bd117e425c71bc65d2860ef5993efa
gignsky <gignsky@gmail.com>
2023-03-24 20:15:09 -0400

    added setup classes workhorse function

--- tdarr-noding/main.py
+++ tdarr-noding/main.py
@@ -8,20 +8,13 @@ import src
 
 def main():
     #establish classes
-    workhorse_class=src.Workhorse()
+    Workhorse=src.Workhorse()
 
     current_directory=os.getcwd()
-    configuration_class=src.Configuration(current_directory)
-
-    server_class=configuration_class.setup_server_class()
-
-    #check if configuration file exists
-    status_exists,status_file=configuration_class.check_if_status_exists()
-
-    status_class=src.StatusTracking(status_file,configuration_class.STATUS_PATH)
+    status_exists=Workhorse.setup_classes(current_directory)
 
     # check if server is online
-    server_status=src.Logic.server_status_check(server_class)
+    server_status=src.Logic.server_status_check(Workhorse.Server)
 
     if server_status=="stop":
         status_class.set_server_status("Offline")
@@ -29,13 +22,13 @@ def main():
         status_class.set_server_status("Online")
 
         #continue establishing classes
-        expected_node_class_dictionary=configuration_class.setup_configuration_node_dictionary()
+        expected_node_class_dictionary=Workhorse.Configuration.setup_configuration_node_dictionary()
 
 
         if status_exists:
             pass
         else:
-            workhorse_class.startup(server_class,expected_node_class_dictionary,configuration_class)
+            Workhorse.startup(server_class,expected_node_class_dictionary,configuration_class)
             status_class.change_state("Started")
 
     status_class.print_server_status()
--- tdarr-noding/src/workhorse_class.py
+++ tdarr-noding/src/workhorse_class.py
@@ -11,7 +11,19 @@ class Workhorse:
     """
 
     # main methods
-    def startup(self,Server,node_dictionary,configuration_class):
+    def setup_classes(self,current_directory):
+        self.Configuration=src.Configuration(current_directory)
+
+        self.Server=self.Configuration.setup_server_class()
+
+        #check if configuration file exists
+        status_exists,self.status_file=self.Configuration.check_if_status_exists()
+
+        self.Status=src.StatusTracking(status_file,self.Configuration.STATUS_PATH)
+
+        return status_exists
+
+    def startup(self,node_dictionary):
         """
         startup function: this will run at the inital start of the script when no status file exists
 
@@ -29,52 +41,52 @@ class Workhorse:
 
         ## 1
         ### 1.a get_nodes output
-        get_nodes_output=tdarr.Tdarr_Logic.generic_get_nodes(Server)
+        get_nodes_output=tdarr.Tdarr_Logic.generic_get_nodes(self.Server)
 
         ### 1.b update configuration class with tdarr info
-        configuration_class.startup_update_nodes_with_tdarr_info(node_dictionary,get_nodes_output)
+        self.Configuration.startup_update_nodes_with_tdarr_info(node_dictionary,get_nodes_output)
 
         ## 2
         quantity_of_living_nodes=999 # set quantity of living nodes to an absurdly high number to allow for looping on next section
 
         ### 2.a - begin looping to kill lowest priority nodes
-        while quantity_of_living_nodes > Server.max_nodes:
+        while quantity_of_living_nodes > self.Server.max_nodes:
             quantity_of_living_nodes = Logic.find_quant_living_nodes(node_dictionary)
 
-            if quantity_of_living_nodes > Server.max_nodes:
+            if quantity_of_living_nodes > self.Server.max_nodes:
                 print(
                     "WARNING: Too many nodes alive; killing last node on the priority list"
                 )
-                highest_priority_node_name=node_interactions.HostLogic.kill_smallest_priority_node(configuration_class,node_dictionary)
+                highest_priority_node_name=node_interactions.HostLogic.kill_smallest_priority_node(self.Configuration,node_dictionary)
 
                 # set node state to offline
                 node_dictionary[highest_priority_node_name].line_state("Offline")
 
         # commented out for new system of work
-        # Logic.reset_node_workers(Server,node_dictionary)
+        # Logic.reset_node_workers(self.Server,node_dictionary)
 
         ## 3
         for node_name in node_dictionary:
             node_class=node_dictionary[node_name]
             if node_class.online:
-                tdarr.Tdarr_Orders.reset_workers_to_zero(Server,node_name,node_dictionary)
+                tdarr.Tdarr_Orders.reset_workers_to_zero(self.Server,node_name,node_dictionary)
 
 
         ## 4
-        _, nodes_without_work_list = tdarr.Tdarr_Logic.find_nodes_with_work(Server)
+        _, nodes_without_work_list = tdarr.Tdarr_Logic.find_nodes_with_work(self.Server)
 
         #shutdown nodes without work
         for node in nodes_without_work_list:
             for node_dict_name in node_dictionary:
                 if node == node_dict_name:
                     #set workers to zero
-                    tdarr.Tdarr_Orders.reset_workers_to_zero(Server,node,node_dictionary)
+                    tdarr.Tdarr_Orders.reset_workers_to_zero(self.Server,node,node_dictionary)
                     #order shutdown
-                    node_interactions.HostLogic.kill_node(configuration_class,node_dictionary,node)
+                    node_interactions.HostLogic.kill_node(self.Configuration,node_dictionary,node)
                     #set node status to offline
                     node_dictionary[node_dict_name].line_state("Offline")
 
-        # primary_node = Server.primary_node
+        # primary_node = self.Server.primary_node
         # primary_node_class = node_dictionary[primary_node]
 
 #         if primary_node_class.online:
@@ -94,8 +106,8 @@ class Workhorse:
 #                 # TODO Same function as above on line 59 looping
 #                 print("PLACEHOLDER")
 
-    def refresh(self,Server):
-        Logic.refresh_all(Server)
+    def refresh(self,self.Server):
+        Logic.refresh_all(self.Server)
 
     # def normal(self):
     #     if self.script_status_file == "Stopped":
